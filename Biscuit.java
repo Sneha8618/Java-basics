5ea75a48cd8b1e8362a8133b8443ddb07722a635
unknown <snehal.xworkz@gmail.com>
2022-09-22 00:12:22 -0700

    committing java files

---
+++ Biscuit.java
@@ -0,0 +1,15 @@
+class Biscuit {
+
+     public static void main (String args[]) {
+
+     String biscuitName = " Parle-G" ;
+     
+     int price = 10 ;
+
+     System.out.println("Biscuit name is " + biscuitName);
+
+     System.out.println("Biscuit price is " + price);
+ 
+     }
+
+}
