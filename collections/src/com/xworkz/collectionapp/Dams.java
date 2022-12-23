8ca7325f2d26fcb6e04f2ee042b12fe285a62f01
unknown <snehal.xworkz@gmail.com>
2022-12-22 20:12:53 -0800

    commiting java files

---
+++ collections/src/com/xworkz/collectionapp/Dams.java
@@ -0,0 +1,277 @@
+package com.xworkz.collectionapp;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class Dams {
+	
+	public static void main(String[] args) {
+		
+		Collection<String> damsInKarnataka = new ArrayList();
+		damsInKarnataka.add("TB");
+		damsInKarnataka.add("KRS");
+		damsInKarnataka.add("Alamatti");
+		damsInKarnataka.add("Harangi");
+		damsInKarnataka.add("Narayanpur");
+	    
+       Collection<String> damsInKeral = new LinkedList();
+       damsInKeral.add("Idukki");
+       damsInKeral.add("Pothundi");
+       damsInKeral.add("Neyyar");
+       damsInKeral.add("Kakki");
+       damsInKeral.add("Pamba");
+       
+       Collection<String> damsInTamilnadu = new ArrayList();
+       damsInTamilnadu.add("Mettur");
+       damsInTamilnadu.add("Vaigi");
+       damsInTamilnadu.add("BhavaniSagar");
+       damsInTamilnadu.add("Amaravathi");
+       damsInTamilnadu.add("Chittar");
+       
+       Collection<String> damsInMaharashtra = new ArrayList();
+       damsInMaharashtra.add("Dhom");
+       damsInMaharashtra.add("Koyna");
+       damsInMaharashtra.add("Warna");
+       damsInMaharashtra.add("Gargoti");
+       damsInMaharashtra.add("Nira");
+       
+       Collection<String> damsInRajasthan = new TreeSet();   
+       damsInRajasthan.add("Mashi");
+       damsInRajasthan.add("RanaPratapSagar");
+       damsInRajasthan.add("Chandelao");
+       damsInRajasthan.add("Mansarovar");
+       damsInRajasthan.add("Mahadev");
+       
+       Collection<String> damsInTelangana = new LinkedList();
+       damsInTelangana.add("Nagarjun");
+       damsInTelangana.add("Sri ram sagar");
+       damsInTelangana.add("Nizam");
+       damsInTelangana.add("Dindi");
+       damsInTelangana.add("Singur");
+       
+	    Collection<String> damsInChhattisgarh = new ArrayList();
+	    damsInChhattisgarh.add("HasdeoBango");
+	    damsInChhattisgarh.add("Gangrel");
+	    damsInChhattisgarh.add("Agariya");
+	    damsInChhattisgarh.add("Tanjara");
+	    damsInChhattisgarh.add("Dudhawa");
+	    
+	    Collection<String> damsInAndhraPradesh = new TreeSet();
+	    damsInAndhraPradesh.add("Srisailm");
+	    damsInAndhraPradesh.add("Somasila");
+	    damsInAndhraPradesh.add("Tatipudi");
+	    damsInAndhraPradesh.add("Gandipalem");
+	    damsInAndhraPradesh.add("Prakasam");
+	    
+	    Collection<String> damsInGujarat = new TreeSet();
+	    damsInGujarat.add("SardarSarovar");
+	    damsInGujarat.add("Ukai");
+	    damsInGujarat.add("Kadana");
+	    damsInGujarat.add("Karjan");
+	
+	    Collection<String> damsInHimachalPradesh = new TreeSet();
+	    damsInHimachalPradesh.add("Bhakra");
+	    damsInHimachalPradesh.add("The Pong");
+	    damsInHimachalPradesh.add("The Chamera");
+	    
+	    Collection<String> damsInJharkhand = new TreeSet();
+	    damsInJharkhand.add("The Maithon");
+	    damsInJharkhand.add("Panchet");
+	    damsInJharkhand.add("Tenughat");
+	    
+	    Collection<String> damsInMadhyaPradesh = new ArrayList();
+	    damsInMadhyaPradesh.add("Gandhi Sagar");
+	    damsInMadhyaPradesh.add("Tawa");
+	    damsInMadhyaPradesh.add("Indira Sagar");
+	    
+	    Collection<String> damsInOdisha = new ArrayList();
+	    damsInOdisha.add("Hirakud");
+	    damsInOdisha.add("Rengali");
+	    damsInOdisha.add("Indaravati");
+	    damsInOdisha.add("The Jalaput");
+	    damsInOdisha.add("Mandir");
+	    
+	    Collection<String> damsInPanjab = new ArrayList();
+	    damsInPanjab.add("The Ranjit Sagar");
+	    damsInPanjab.add("Dholbaha");
+	    damsInPanjab.add("Jainti");
+	    damsInPanjab.add("Damsal");
+	    damsInPanjab.add("Chohal");
+	    
+	    Collection<String> damsInUttarkhand = new ArrayList();
+	    damsInUttarkhand.add("Ramganga");
+	    damsInUttarkhand.add("Jamrani");
+	    damsInUttarkhand.add("The Tehri");
+	    damsInUttarkhand.add("Haripura");
+	    damsInUttarkhand.add("Bhalu");
+	    
+	    Collection<String> damsInUttarPradesh = new ArrayList();
+	    damsInUttarPradesh.add("Rihand");
+	    damsInUttarPradesh.add("Matatila");
+	    damsInUttarPradesh.add("The Rajghat");
+	    damsInUttarPradesh.add("Bansagar");
+	    damsInUttarPradesh.add("Idukki");
+	    
+	    Collection<String> damsInManipur = new ArrayList();
+	    damsInManipur.add("Khuga");
+	    damsInManipur.add("Tipaimukh");
+	    damsInManipur.add("Maphou");
+	    damsInManipur.add("Singda");
+	    damsInManipur.add("Keirak");
+	    
+	    Collection<String> damsInMizoram = new ArrayList();
+	    damsInMizoram.add("Tuirial");
+	    damsInManipur.add("Serlui");
+	    damsInManipur.add("Tuirial");
+	    damsInManipur.add("Bairabi");
+	    
+	    Collection<String> damsInMeghalaya = new ArrayList();
+	    damsInMeghalaya.add("Khandong");
+	    damsInMeghalaya.add("Kyrdemukulai");
+	    damsInMeghalaya.add("Mawphlang");
+	    damsInMeghalaya.add("Myntdu-Leshka");
+	    
+	    Collection<String> damsInDelhi = new ArrayList();
+	    damsInDelhi.add("Hartwick");
+	    damsInDelhi.add("Serlui");
+	    damsInDelhi.add("Tuirial");
+	    damsInDelhi.add("Bairabi");
+	    
+	    Collection<String> damsInGoa = new ArrayList();
+	    damsInGoa.add("Anjunam");
+	    damsInGoa.add("M.I");
+	    damsInGoa.add("Salaulim");
+	   
+	    Collection<String> damsInArunachalPradesh = new ArrayList();
+	    damsInArunachalPradesh.add("Ranganadi");
+	    damsInArunachalPradesh.add("Subansiri");
+	    
+	    Collection<String> damsInNagaland = new ArrayList();
+	    damsInNagaland.add("Hep");
+	    
+	    Collection<String> damsInAssam = new ArrayList();
+	    damsInAssam.add("Karbi Langpi");
+	    damsInAssam.add("Umrong");
+	    
+	    Collection<String> damsInTripura = new ArrayList();
+	    damsInTripura.add("Gumti Hydro");
+	    
+	    Collection<String> damsInWestBengal = new ArrayList();
+	    damsInWestBengal.add("Bakreshwar");
+	    damsInWestBengal.add("Bandhu");
+	    damsInWestBengal.add("Bara Mandira");
+	    damsInWestBengal.add("Barabhum");
+	    damsInWestBengal.add("Beko");
+	    
+	    Collection<String> damsInSikkim = new LinkedList();
+	    damsInSikkim.add("Bakreshwar");
+	    damsInSikkim.add("Bandhu");
+	    damsInSikkim.add("Bara Mandira");
+	    damsInSikkim.add("Barabhum");
+	    damsInSikkim.add("Beko");
+	    
+	    Collection<String> damsInBihar = new LinkedList();
+	    damsInBihar.add("Ajan");
+	    damsInBihar.add("Amrity");
+	    damsInBihar.add("Badua");
+	    damsInBihar.add("Durgawati");
+	    damsInBihar.add("Job");
+	    
+	   Collection<String> damsInIndia = new LinkedList();
+	   damsInIndia.addAll(damsInKarnataka);
+	   damsInIndia.addAll(damsInKeral);
+	   damsInIndia.addAll(damsInTamilnadu);
+	   damsInIndia.addAll(damsInMaharashtra);
+	   damsInIndia.addAll(damsInRajasthan);
+	   damsInIndia.addAll(damsInTelangana);
+	   damsInIndia.addAll(damsInChhattisgarh);
+	   damsInIndia.addAll(damsInAndhraPradesh);
+	   damsInIndia.addAll(damsInHimachalPradesh);
+	   damsInIndia.addAll(damsInUttarPradesh);
+	   damsInIndia.addAll(damsInPanjab);
+	   damsInIndia.addAll(damsInMizoram);
+	   damsInIndia.addAll(damsInManipur);
+	   damsInIndia.addAll(damsInTripura);
+	   damsInIndia.addAll(damsInSikkim);
+	   damsInIndia.addAll(damsInMeghalaya);
+	   damsInIndia.addAll(damsInDelhi);
+	   damsInIndia.addAll(damsInGoa);
+	   damsInIndia.addAll(damsInBihar);
+	   damsInIndia.addAll(damsInAssam);
+	   damsInIndia.addAll(damsInNagaland);
+	   damsInIndia.addAll(damsInUttarkhand);
+	   damsInIndia.addAll(damsInPanjab);
+	   damsInIndia.addAll(damsInOdisha);
+	   damsInIndia.addAll(damsInMadhyaPradesh);
+	   damsInIndia.addAll(damsInGujarat);
+	   damsInIndia.addAll(damsInJharkhand);
+	   
+	   System.out.println(damsInIndia);
+	   System.out.println(damsInIndia.size());
+	   
+	   for(String dams : damsInIndia) {
+		   System.out.println(dams);
+	   }
+	   
+	   System.out.println("Accessed using iterator()");
+	   Iterator<String> dams = damsInIndia.iterator();
+	   while(dams.hasNext()) {
+		   String ref = dams.next();
+		   System.out.println(ref);
+	   
+		   System.out.println("dams name start with 't' ");
+		   if(ref.startsWith("T")) {
+			   System.out.println(ref);
+		}
+		   
+		   System.out.println("dams name ends with 'ra' ");
+		   if(ref.endsWith("ra")) {
+			   System.out.println(ref);
+			   
+		   }
+		   
+		   if(ref.length() > 15) {
+			   System.out.println("the elements lenth greater than 15 " + ref);
+		   }
+		   
+		   if(ref.contains("p")) {
+			  System.out.println("contains p value removes" + ref);
+			  dams.remove();
+		   }
+		   
+		   String upper = ref.toUpperCase();
+		   System.out.println(upper);
+		   
+		   String lower = ref.toLowerCase();
+		   System.out.println(lower);
+		   
+	   }
+	}
+	
+	  /* System.out.println("*************************");
+	   System.out.println("remove() method ");
+	   
+		boolean ref1 = damsInIndia.remove("KRS");
+		System.out.println(ref1);
+		
+		boolean ref2 = damsInIndia.remove("Bandhu");
+		System.out.println(ref2);
+			   
+	   System.out.println("************************");
+	   System.out.println("contains() method");
+	   
+	   boolean ref3 = damsInIndia.contains("Nira");
+	   System.out.println(ref3);
+	   
+	   boolean ref4 = damsInIndia.contains("Sneha");
+	   System.out.println(ref4);
+	   
+	   boolean ref5 = damsInIndia.contains("Bhalu");
+	   System.out.println(ref5); */
+	   
+      }
+
+
