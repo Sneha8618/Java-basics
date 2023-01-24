6741e86546dcf1d6fc78ccc0d8051299daadcaf5
unknown <snehal.xworkz@gmail.com>
2023-01-23 21:42:40 -0800

    commiting files

---
+++ servlet/tetra/src/main/java/com/xworkz/terta/MountainServlet.java
@@ -0,0 +1,25 @@
+package com.xworkz.terta;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(loadOnStartup = 8, urlPatterns = "/nandi")
+
+public class MountainServlet extends HttpServlet {
+	
+	public MountainServlet() {
+			System.out.println("Created " + this.getClass().getSimpleName());
+		}
+		
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			System.out.println("Running doGet method in FastTrackServlet...");
+			super.doGet(req, resp);
+		}
+
+}
